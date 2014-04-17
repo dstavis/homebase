0111ce8347984ff0ae8d9b2fcd587e7d4eb4a7ab
David Stavis <davidstavis@gmail.com>
2014-04-16 20:54:22 -0700

    Initial commit

---
+++ index.rb
@@ -0,0 +1,3 @@
+#Write the GET route for "join group" links
+#Should create a session key containing the id of the group the user has permission to join
+#During the create account flow, if this key has a value in the session, the user should automatically be added to the group corresponding with this value
