f5c2709810a3c5c8a367b27ee31dde3664af4101
David Stavis <davidstavis@gmail.com>
2014-04-18 15:36:10 -0700

    Pops up send invite with poorly formatted link

--- public/js/application.js
+++ public/js/application.js
@@ -41,6 +41,8 @@ var addEventListeners = function () {
             });
         }
     });
+
+    $('.invite').click(sendInvite)
 };
 
 
@@ -65,4 +67,14 @@ var loginOrCreateUser = function(userData){
     }).done(function (userPage) {
         window.location.href = userPage;
     });
+}
+
+var sendInvite = function(e){
+     groupURL = "http://localhost:9393/join/group/"+current_user_group_id
+     
+  	debugger
+		FB.ui({
+	  		method: 'send',
+	  		link: groupURL,
+		});
 }
