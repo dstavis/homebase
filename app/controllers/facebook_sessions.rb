313cd033c0eb501ea502c3c8b9608a819cc57def
David Stavis <davidstavis@gmail.com>
2014-04-18 15:08:13 -0700

    Automatically adds a group id to a user if they login or create account through facebook and have navigated to the group's join link in this session.

--- app/controllers/facebook_sessions.rb
+++ app/controllers/facebook_sessions.rb
@@ -4,8 +4,19 @@ post '/session/facebook_login/' do
 	else
 		user = User.create(email: params['email'], facebook_id: params['facebook_id'], picture_url: params['picture_url'])
 	end
+
+	if session[:join_group]
+		user.group_id = session[:join_group]
+		user.save
+		session[:join_group] = nil
+	end
 	
 	session[:user_id] = user.id
 
 	return "/user/#{user.id}"
+end
+
+get '/join/group/:id' do
+	session[:join_group] = params[:id]
+	redirect '/session/login'
 end
