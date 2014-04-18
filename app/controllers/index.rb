62dd9698873d515a126542ae57c404b1a1c324fc
Ashwin Kumar <ash.nkumar@gmail.com>
2014-04-18 13:54:55 -0700

    almost finished for pres

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,6 +3,11 @@ get '/' do
   erb :login
 end
 
+post '/logout' do
+	session.clear
+	erb :login
+end
+
 #Write the GET route for "join group" links
 #Should create a session key containing the id of the group the user has permission to join
 #During the create account flow, if this key has a value in the session, the user should automatically be added to the group corresponding with this value
